34aea73fca46710b52f6d911bbe5de845825f2a2
Gerardo Soriano <gerardosoriano97@gmail.com>
2017-05-20 04:19:24 -0500

    Se medio corrigen los paths en los html de las noticias

---
+++ web/news/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('div.outstanding').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+});
